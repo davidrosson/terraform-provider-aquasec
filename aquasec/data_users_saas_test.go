f7bc16af14364e2a74bfcb356462ec8a74a17556
yossigilad-aquasec <yossi.gilad@aquasec.com>
2022-03-06 11:24:39 +0200

    Adding support for saas users and groups data\resource

---
+++ aquasec/data_users_saas_test.go
@@ -0,0 +1,43 @@
+package aquasec
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAquasecUserSaasManagementDatasource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAquasecUserSaasDataSource(),
+				Check:  testAccCheckAquasecUsersSaasDataSourceExists("data.aquasec_users_saas.testusers"),
+			},
+		},
+	})
+}
+
+func testAccCheckAquasecUserSaasDataSource() string {
+	return `
+	data "aquasec_users_saas" "testusers" {}
+	`
+}
+
+func testAccCheckAquasecUsersSaasDataSourceExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+
+		if !ok {
+			return NewNotFoundErrorf("%s in state", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return NewNotFoundErrorf("Id for %s in state", n)
+		}
+
+		return nil
+	}
+}
